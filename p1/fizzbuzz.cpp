e7c77cba840138b7b804f99401551f6af3bb4725
Austin Ward <zorro@Austins-MacBook-Pro.local>
2016-02-25 23:56:34 -0500

    First problem done

---
+++ p1/fizzbuzz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int sum = 0;
+
+    cout << "Enter natural number: ";
+    cin >> num;
+
+    for(int i = 3; i < num; i++) {
+        if(i % 3 == 0 || i % 5 == 0) sum += i; cout << i << " " << sum << endl;
+    }
+
+    cout << "The sum of all the Natural Numbers under " << num << " is: " << sum << endl;
+}
