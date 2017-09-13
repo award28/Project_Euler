304e8566901651552b2edb1e02a7bd0566ebb5a5
award28 <ward.austin28@gmail.com>
2017-09-13 13:27:51 -0400

    Added p32

---
+++ p32/pandigitalProducts.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//int numberLetterCounter(int range);
+
+int main() {
+    bool finished = false;
+    int x = 1;
+    int y = 2;
+    int z = 2;
+    string 
+    while(!finished) {
+        x = getUniqueNum(x);
+        y = getUniqueNum(y);
+        z = x * y;
+         
+    }
+}
+
+//int numberLetterCounter(int range) {
+
+/*
+ * Functions to be made:
+ *  Are two ints different and each char unique? 
+ *  Take two ints and their product, put them in a string. Is it exactly 9 digits long?
+ *  Does this contain 1 - 9?
+ *  
+ */
